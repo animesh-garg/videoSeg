78812a5e33655e88215d3ad9256cba61958cf9a4
jeff <jmahler@berkeley.edu>
2013-11-24 17:41:55 -0800

    Added files to be filled in for organizational purposes

---
+++ src/propagate_labels.m
@@ -0,0 +1,7 @@
+function [ output_args ] = propagate_flows( input_args )
+%PROPAGATE_FLOWS Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ src/visualize_result.m
@@ -0,0 +1,7 @@
+function [ output_args ] = visualize_result( input_args )
+%VISUALIZE_RESULT Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
