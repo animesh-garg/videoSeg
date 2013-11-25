0382733340697e8409e252b2c5f3def9b4e45ebb
Shubham Tulsiani <shubhtuls@gmail.com>
2013-11-24 16:47:13 -0800

    Corrected typo

--- src/startup.m
+++ src/startup.m
@@ -1,2 +1,2 @@
 datasetPath = '~/Work/Vision/Datasets/moseg_dataset/';
-b = 2; %Neighbourhood Sizr for next frame in which current pixel woul map to
+b = 2; %Neighbourhood Size for next frame in which current pixel woul map to
